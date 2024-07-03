7972a74427ebed70139c9255076e109da1311344
DurgaNaresh <durgaanarresh@gmail.com>
2024-07-03 10:22:50 +0530

    added for loop

--- Test.java
+++ Test.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Good Morning -- branch1");
+        List<String> list = Arrays.asList(new String[]{"Raghu", "Ram"});
+        for (String text: list) {
+            System.out.println(text);
+        }
     }
 }
