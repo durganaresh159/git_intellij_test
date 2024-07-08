6d57e269f5379a1d6dc4642aba12dc14918a446a
DurgaNaresh <durgaanarresh@gmail.com>
2024-07-08 14:13:57 +0530

    added for loop

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
+        System.out.println("I am learning spring batch");
         System.out.println("Hello Good Morning");
     }
 }
