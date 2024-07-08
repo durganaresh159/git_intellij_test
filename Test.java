2ad5d016e06c88c30912765ce00707d06bfb2d3e
DurgaNaresh <durgaanarresh@gmail.com>
2024-07-08 14:13:57 +0530

    added for loop

--- Test.java
+++ Test.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println("I am learning spring batch");
         System.out.println("Hello Good Morning");
         int x=10;
         System.out.println("branch 2 is created");
